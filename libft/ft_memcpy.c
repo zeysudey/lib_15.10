d67cece4ee069b64a4221354cfde75b4689a79fd
Zeynep Sude Yılmaz <97186117+zeysudey@users.noreply.github.com>
2024-10-20 21:29:55 +0300

    Update ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -15,7 +15,7 @@ void * ft_memcpy(void * dst, const void * src, size_t n){
     int i;
 
     i = 0;
-    if(dst == NULL && src == NULL)
+    if(dst == NULL || src == NULL)**
         return 0;
     while(i<n){
         *((unsigned char *)dst) = *((unsigned char *)src);
